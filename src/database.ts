ca2bb8c10a42cdbabb1443768f5a50a2e7191d75
thiago lopes de mello <timabuntu@gmail.com>
2023-03-13 19:07:56 -0300

    feat: migration

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,10 @@
 import { knex as setupKnex } from 'knex'
-
-export const knex = setupKnex({
+const config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/app.db',
   },
-})
+  useNullAsDefault: true,
+}
+
+export const knex = setupKnex(config)
