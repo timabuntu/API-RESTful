2c160ae33f8a83ebd1c0289bad36439995338a84
thiago lopes de mello <timabuntu@gmail.com>
2023-03-14 18:16:32 -0300

    feat: created querie select amount value

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions').select('*')
-
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
   return transaction
 })
-
 app
   .listen({
     port: 3333,
