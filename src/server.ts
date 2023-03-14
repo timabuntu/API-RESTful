e03a3167683326c6b3855af328153ddc0b2a0f22
thiago lopes de mello <timabuntu@gmail.com>
2023-03-14 18:12:41 -0300

    feat: created  select all

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
+// import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
+  const transaction = await knex('transactions').select('*')
 
   return transaction
 })
