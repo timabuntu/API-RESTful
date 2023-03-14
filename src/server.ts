7f2d6ca7a1eb89238fbad7b7c066199a942a6dee
thiago lopes de mello <timabuntu@gmail.com>
2023-03-14 18:09:48 -0300

    feat: created insertion table

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app
